7713510e507d61df0df343f3a849bab7781540eb
‘Mohamed <megypt860@gmail.com>
2024-03-08 22:16:47 +0100

    fteched pet data

--- main.js
+++ main.js
@@ -3,7 +3,17 @@ async function start() {
     const weatherData = await weatherPromise.json()
     const ourTemperature = weatherData.properties.periods[0].temperature;
     document.querySelector("#temperature-output").textContent = ourTemperature;
-    console.log(ourTemperature)
 }
 
 start()
+
+
+async function petsArea() {
+    const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json");
+    const petsDate = await petsPromise.json()
+     petsDate.forEach((data) => {
+        console.log(data)
+     })
+}
+
+petsArea()
